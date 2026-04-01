c7ff4c31085f0cee5084b9d4573240e8b4d43ba5
poncha kiệt <tuankiet3459@gmail.com>
2026-04-02 02:34:52 +0700

    ss3

--- bt-ss4/src/main/java/poncha/kiet/ss4/controller/InstructorController.java
+++ bt-ss4/src/main/java/poncha/kiet/ss4/controller/InstructorController.java
@@ -3,14 +3,13 @@ package poncha.kiet.ss4.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import poncha.kiet.ss4.entity.ApiResponse;
 import poncha.kiet.ss4.entity.Instructor;
 import poncha.kiet.ss4.entity.InstructorCreateRequest;
 import poncha.kiet.ss4.service.InstructorService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/instructors")
@@ -24,6 +23,11 @@ public class InstructorController {
     @PostMapping
     public ResponseEntity<ApiResponse<Instructor>> createInstructor(@RequestBody InstructorCreateRequest instructor){
         instructorService.createInstructor(instructor);
-        return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>(true,"Course created successfully!"));
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>(true,"Instructor created successfully!"));
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<Instructor>>> getAllInstructors(){
+        return ResponseEntity.ok(new ApiResponse<>(true,"Instructor list successfully!", instructorService.findAllInstructors()));
     }
 }
